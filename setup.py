656cb765554cef8c457def3fa0833caba548f789
ryokamoi <ryokamoi.jp@gmail.com>
2023-09-24 19:57:40 -0400

    updated easy_io version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="openai_api_wrapper",
-    version="0.4.0",
+    version="0.4.1",
     description="This project includes an unofficial OpenAI API wrapper.",
     author="Ryo Kamoi",
     author_email="ryokamoi.jp@gmail.com",
     url="https://github.com/ryokamoi/openai_api_wrapper",
     license="MIT",
     python_requires=">=3.9",
-    install_requires=["openai", "easy_io @ git+ssh://git@github.com/ryokamoi/easy_io@0.1.1"]
+    install_requires=["openai", "easy_io @ git+ssh://git@github.com/ryokamoi/easy_io@0.2.1"]
 )
